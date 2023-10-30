b753b26af9410c8006f01036efc7cce232958417
Katjahkrogh <you@example.com>
2023-10-30 15:29:50 +0100

    css modules footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,12 +1,14 @@
+import styles from "./Footer.module.css";
+
 function Footer(props) {
   const companies = ["Matas", "Forte Advice", "LEGO"];
   return (
-    <footer>
+    <footer className={styles.footer}>
       <p>
         Her er en meget standard footer hvor jeg gerne vil vise (
         {props.contactInfo})
       </p>
-      <ul>
+      <ul className={styles.ul}>
         {companies.map((comp) => (
           <Client company={comp} />
         ))}
